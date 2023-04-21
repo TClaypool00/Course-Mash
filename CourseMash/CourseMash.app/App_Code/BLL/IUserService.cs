6b72b5f8ec51707e003176a20dc242462b0a57a2
TClaypool00 <claypooltyler00@gmail.com>
2023-04-20 22:48:54 -0500

    User service only create and get by email

---
+++ CourseMash/CourseMash.app/App_Code/BLL/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using CourseMash.app.Models;
+
+namespace CourseMash.app.App_Code.BLL
+{
+    public interface IUserService
+    {
+        public Task<List<UserViewModel>> GetUsersAsync();
+
+        public Task<UserViewModel> GetUserAsync(int id);
+
+        public Task<UserViewModel> GetUserByEmailAsync(string email);
+
+        public Task AddUserAsync(RegisterViewModel model);
+
+        public Task UpdateUserAsync(UserViewModel model);
+
+        public Task<bool> UserExistsById(int id);
+
+        public Task<bool> UserExistsByEmailAsync(string email);
+
+        public Task<bool> UserExistsByPhoneNumbAsync(string phoneNumb);
+    }
+}
---
+++ CourseMash/CourseMash.app/App_Code/DAL/UserService.cs
@@ -0,0 +1,66 @@
+﻿using CourseMash.app.App_Code.BLL;
+using CourseMash.app.App_Code.BOL;
+using CourseMash.app.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseMash.app.App_Code.DAL
+{
+    public class UserService : IUserService
+    {
+        private readonly CourseMashContext _context;
+
+        public UserService(CourseMashContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(RegisterViewModel model)
+        {
+            var dataModel = Mapper.MapUser(model);
+
+            await _context.Users.AddAsync(dataModel);
+
+            await SaveAsync();
+        }
+
+        public Task<UserViewModel> GetUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserViewModel> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserViewModel>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserAsync(UserViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsByEmailAsync(string email)
+        {
+            return _context.Users.AnyAsync(u => u.Email == email);
+        }
+
+        public Task<bool> UserExistsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsByPhoneNumbAsync(string phoneNumb)
+        {
+            return _context.Users.AnyAsync(u => u.PhoneNumb == phoneNumb);
+        }
+
+        private async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
