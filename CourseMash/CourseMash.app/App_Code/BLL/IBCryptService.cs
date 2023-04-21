85fd97e5461e83d6b228d0c38f44559f1816ddb9
TClaypool00 <claypooltyler00@gmail.com>
2023-04-20 22:49:34 -0500

    Added service for BCrypt

---
+++ CourseMash/CourseMash.app/App_Code/BLL/IBCryptService.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseMash.app.App_Code.BLL
+{
+    public interface IBCryptService
+    {
+        public string HashPassword(string text);
+
+        public bool VerifyPassword(string text, string hash);
+    }
+}
---
+++ CourseMash/CourseMash.app/App_Code/DAL/BCryptService.cs
@@ -0,0 +1,17 @@
+﻿using CourseMash.app.App_Code.BLL;
+
+namespace CourseMash.app.App_Code.DAL
+{
+    public class BCryptService : IBCryptService
+    {
+        public string HashPassword(string text)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(text);
+        }
+
+        public bool VerifyPassword(string text, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(text, hash);
+        }
+    }
+}
