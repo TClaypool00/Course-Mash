f208b98f645495a731b244ff1eabeb0625c92cf0
TClaypool00 <claypooltyler00@gmail.com>
2023-04-20 23:00:33 -0500

    Created mapper class to convert RegisterViewModel to User data model

---
+++ CourseMash/CourseMash.app/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using CourseMash.app.App_Code.BOL;
+using CourseMash.app.Models;
+
+namespace CourseMash.app
+{
+    public class Mapper
+    {
+        public static User MapUser(RegisterViewModel model)
+        {
+            return new User
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                Password = model.Password,
+                IsAdmin = model.IsAdmin,
+                PhoneNumb = model.PhoneNumb
+            };
+        }
+    }
+}
